f5a779af37ed1a32eb5252667136e2922eb6c539
MashleMashleMashleMashle <nicoangeloilustre@gmail.com>
2024-11-08 00:45:47 +0800

    adjusted wsgi file

--- Prototype/EngiNeed/EngiNeed/wsgi.py
+++ Prototype/EngiNeed/EngiNeed/wsgi.py
@@ -1,17 +1,12 @@
-"""
-WSGI config for EngiNeed project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 from django.core.wsgi import get_wsgi_application
 
-# Set the default settings module for the 'EngiNeed' project.
+# Add the Prototype directory to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+# Set the settings module for the project
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EngiNeed.settings')
 
-# Expose the WSGI application callable
+# Initialize the WSGI application
 application = get_wsgi_application()
