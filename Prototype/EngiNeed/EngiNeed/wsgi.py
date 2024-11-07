b1fea839dac2e4321f9d06293b7ea3c0bba536d3
MashleMashleMashleMashle <nicoangeloilustre@gmail.com>
2024-11-08 01:05:22 +0800

    last ocmmit of the night

--- Prototype/EngiNeed/EngiNeed/wsgi.py
+++ Prototype/EngiNeed/EngiNeed/wsgi.py
@@ -2,11 +2,14 @@ import os
 import sys
 from django.core.wsgi import get_wsgi_application
 
-# Add the Prototype directory to the Python path
-sys.path.append(r'C:\Users\NanetteBugna\Desktop\Site Development\Prototype')
 
 # Set the Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EngiNeed.settings')
 
 # Initialize the WSGI application
 application = get_wsgi_application()
+
+
+sys.path.append('C:\Users\NanetteBugna\Desktop\Site Development\Prototype')  # Adjust this to the correct path
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EngiNeed.settings')
+
