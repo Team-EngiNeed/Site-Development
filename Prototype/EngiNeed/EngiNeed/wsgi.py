d5798e7771d0dc9ba734a5fae04a5db5ead8dbeb
MashleMashleMashleMashle <nicoangeloilustre@gmail.com>
2024-11-07 23:46:20 +0800

    added waitress

--- Prototype/EngiNeed/EngiNeed/wsgi.py
+++ Prototype/EngiNeed/EngiNeed/wsgi.py
@@ -7,10 +7,22 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
+
+
 import os
 
-from django.core.wsgi import get_wsgi_application
+# Check if the platform is Windows and import the appropriate server library
+if os.name == 'nt':  # Windows
+    from waitress import serve
+else:  # Unix-based system (Linux/MacOS)
+    from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EngiNeed.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EngiNeed.settings')
 
-application = get_wsgi_application()
+    application = get_wsgi_application()
+
+    # If you are using Gunicorn, your system will automatically use it as long as
+    # the gunicorn command is used during deployment or local server execution.
+    # Example: gunicorn EngiNeed.wsgi:application --bind 0.0.0.0:8000
+
+    # No changes are needed if you're using Gunicorn on Unix-based systems.
