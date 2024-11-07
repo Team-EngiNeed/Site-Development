f064f1e32f735888c446fbdddcb819ac67136cd6
MashleMashleMashleMashle <nicoangeloilustre@gmail.com>
2024-11-08 00:34:04 +0800

    changed wsgi to its glory

--- Prototype/EngiNeed/EngiNeed/wsgi.py
+++ Prototype/EngiNeed/EngiNeed/wsgi.py
@@ -7,23 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
-
-
 import os
+from django.core.wsgi import get_wsgi_application
 
-# Check if the platform is Windows and import the appropriate server library
-if os.name == 'nt':  # Windows
-    from waitress import serve
-else:  # Unix-based system (Linux/MacOS)
-    from django.core.wsgi import get_wsgi_application
+# Set the default settings module for the 'EngiNeed' project.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EngiNeed.settings')
 
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EngiNeed.settings')
-
-    application = get_wsgi_application()
-
-    # If you are using Gunicorn, your system will automatically use it as long as
-    # the gunicorn command is used during deployment or local server execution.
-    # Example: gunicorn EngiNeed.wsgi:application --bind 0.0.0.0:8000
-
-    # No changes are needed if you're using Gunicorn on Unix-based systems.
-
+# Expose the WSGI application callable
+application = get_wsgi_application()
