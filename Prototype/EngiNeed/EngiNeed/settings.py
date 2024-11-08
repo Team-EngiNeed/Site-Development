e82728d5981963fe6c643ffea4a929ac76628df1
MashleMashleMashleMashle <nicoangeloilustre@gmail.com>
2024-11-08 19:02:21 +0800

    fix #7, changed the settings directory again

--- Prototype/EngiNeed/EngiNeed/settings.py
+++ Prototype/EngiNeed/EngiNeed/settings.py
@@ -133,7 +133,7 @@ STATICFILES_DIRS = [
 ]
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
